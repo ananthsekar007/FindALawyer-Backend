7a073e31ea9067b909a628a28434fa8e58b8a09b
ananthsekar007 <ananthsekar007@gmail.com>
2023-06-26 19:12:40 +0530

    Added a Dao for login input

---
+++ Dao/LoginInput.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FindALawyer.Dao
+{
+    public class LoginInput
+    {
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
