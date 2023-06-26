cdc5112858b5930c59b6c7a3f315f2c8ca351d1c
ananthsekar007 <ananthsekar007@gmail.com>
2023-06-26 18:38:23 +0530

    Added Model for client table

---
+++ Models/Client.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace FindALawyer.Models
+{
+    [Table("client")]
+    public class Client
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ClientId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        [EmailAddress] 
+        public string EmailAddress { get; set;} = string.Empty;
+
+        [MaxLength(10)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public byte[] PasswordHash { get; set; }
+
+        [JsonIgnore]
+        public byte[] PasswordSalt { get; set; }
+
+        public string Address { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+
+    }
+}
