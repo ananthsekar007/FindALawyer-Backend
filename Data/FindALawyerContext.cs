7e5f05dcdc260fcff2e7257fc2b45321800d3e34
ananthsekar007 <ananthsekar007@gmail.com>
2023-06-26 18:43:03 +0530

    Added migration for lawyer table in database

--- Data/FindALawyerContext.cs
+++ Data/FindALawyerContext.cs
@@ -11,5 +11,7 @@ namespace FindALawyer.Data
         }
 
         public DbSet<Client> Client { get; set; } = default!;
+
+        public DbSet<Lawyer> Lawyer { get; set; } = default!;
     }
 }
---
+++ Migrations/20230626131205_AddLawyerTable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindALawyer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLawyerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "lawyer",
+                columns: table => new
+                {
+                    LawyerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Qualification = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_lawyer", x => x.LawyerId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "lawyer");
+        }
+    }
+}
