b726b2b71bcbe4733df43b179945959e57db6f34
ananthsekar007 <ananthsekar007@gmail.com>
2023-06-26 18:23:52 +0530

    Added generic app user class

---
+++ Dao/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace FindALawyer.Dao
+{
+    public class AppUser
+    {
+        public string Email { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+    }
+}
