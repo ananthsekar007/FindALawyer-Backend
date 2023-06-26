57e49ad85e22dd662da6941ecb4523cc5dacaac7
ananthsekar007 <ananthsekar007@gmail.com>
2023-06-26 18:24:06 +0530

    Added jwt service implementation for creating a token

---
+++ Services/JwtService/IJwtService.cs
@@ -0,0 +1,13 @@
+﻿using FindALawyer.Dao;
+
+namespace FindALawyer.Services.JwtService
+{
+    public interface IJwtService
+    {
+        public interface IJwtService
+        {
+            string CreateToken(AppUser user);
+        }
+    }
+}
+
