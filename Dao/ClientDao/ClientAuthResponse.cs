b3b970d8519be59150a6a94425cfbb23ccf8d54e
ananthsekar007 <ananthsekar007@gmail.com>
2023-06-26 19:07:45 +0530

    Added Dao class for client auth response

---
+++ Dao/ClientDao/ClientAuthResponse.cs
@@ -0,0 +1,11 @@
+﻿using FindALawyer.Models;
+
+namespace FindALawyer.Dao.ClientDao
+{
+    public class ClientAuthResponse
+    {
+        public Client Client { get; set; } = new Client();
+
+        public string AuthToken { get; set; } = string.Empty;
+    }
+}
