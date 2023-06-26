eba11c0d2b4075b75518c1992555c4b0035ac58f
ananthsekar007 <ananthsekar007@gmail.com>
2023-06-26 18:42:45 +0530

    Added model for Lawyer table

---
+++ Models/Lawyer.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace FindALawyer.Models
+{
+    [Table("lawyer")]
+    public class Lawyer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LawyerId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string EmailAddress { get; set; } = string.Empty;
+
+        [MaxLength(10)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Qualification { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public byte[] PasswordHash { get; set; }
+
+        [JsonIgnore]
+        public byte[] PasswordSalt { get; set; }
+
+        public string Type { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
