a8a294c390dc36eb570dac9f358064c0994bff0d
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-03 11:19:23 +0530

    Added lawyer auth input dao class

---
+++ Dao/LawyerDao/LawyerAuthInput.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FindALawyer.Dao.LawyerDao
+{
+    public class LawyerAuthInput
+    {
+        public string Name { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string EmailAddress { get; set; } = string.Empty;
+
+        [MaxLength(10)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+    }
+}
