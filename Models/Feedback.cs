9e755239f79678cecb5e2cad4d20ecc2114e014a
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-05 10:57:57 +0530

    Added feedback model and migrated the table to database

---
+++ Models/Feedback.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FindALawyer.Models
+{
+    [Table("feedback")]
+    public class Feedback
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int FeedbackId { get; set; }
+
+        [ForeignKey("Lawyer")]
+        public int LawyerId { get; set; }
+
+        [ForeignKey("Client")]
+        public int ClientId { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Remarks { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public virtual Lawyer Lawyer { get; set; }
+        public virtual Client Client { get; set; }
+    }
+}
