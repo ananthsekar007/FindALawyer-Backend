a8541c17e40b4b66d0f488c2af97843be7a160d9
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-05 16:19:44 +0530

    Added client service and its implementation to check if the client is valid

---
+++ Services/ClientService/ClientServiceImpl.cs
@@ -0,0 +1,20 @@
+﻿using FindALawyer.Data;
+using FindALawyer.Models;
+
+namespace FindALawyer.Services.ClientService
+{
+    public class ClientServiceImpl : IClientService
+    {
+        private readonly FindALawyerContext _context;
+        public ClientServiceImpl(FindALawyerContext context) { 
+            _context = context;
+        }
+
+        public async Task<bool> IsValidClient(int clientId)
+        {
+            Client existingClient = await _context.Client.FindAsync(clientId);
+            if (existingClient == null) return false;
+            return true;
+        }
+    }
+}
---
+++ Services/ClientService/IClientService.cs
@@ -0,0 +1,7 @@
+﻿namespace FindALawyer.Services.ClientService
+{
+    public interface IClientService
+    {
+        Task<bool> IsValidClient(int clientId);
+    }
+}
