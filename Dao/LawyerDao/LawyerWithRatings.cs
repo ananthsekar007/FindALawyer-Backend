8526496a6c39e339de2508c6d51b0aacc18418f9
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-05 10:57:06 +0530

    Added a dao for lawyers with ratings

---
+++ Dao/LawyerDao/LawyerWithRatings.cs
@@ -0,0 +1,19 @@
+﻿using FindALawyer.Models;
+
+namespace FindALawyer.Dao.LawyerDao
+{
+    public class LawyerWithRatings
+    {
+        public int LawyerId { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Qualification { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public float AverageRating { get; set; }
+        public ICollection<Feedback> Feedbacks { get; set; }
+    }
+}
