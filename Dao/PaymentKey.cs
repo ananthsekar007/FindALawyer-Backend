a6115022b51bce3d2be1c77e6972dbcd387e7565
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-10 19:03:22 +0530

    Added Dao class for payment keys

---
+++ Dao/PaymentKey.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace FindALawyer.Dao
+{
+    public class PaymentKey
+    {
+        private readonly IConfiguration _configuration;
+        public PaymentKey(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            Key = _configuration.GetSection("PaymentSettings:KEY").Value;
+            Secret = _configuration.GetSection("PaymentSettings:SECRET").Value;
+        }
+
+        public string Key { get; private set; }
+        public string Secret { get; private set; }
+    }
+}
+
