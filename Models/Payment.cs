63cf6ef270f644bab43dfe7f07c624e57f411f1e
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-10 19:03:38 +0530

    Added tables to handle payments

---
+++ Models/Payment.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FindALawyer.Models
+{
+    [Table("payments")]
+    public class Payment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PaymentId { get; set; }
+
+        [ForeignKey("Appointment")]
+        public int AppointmentId { get; set; }
+
+        public float Amount { get; set; }
+
+        public string Status { get; set; } = "PENDING";
+
+        [AllowNull]
+        [ForeignKey("RazorPayments")]
+        public int? payment_reference_id { get; set; } = null;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public virtual Appointment Appointment { get; set; }
+        public virtual RazorPayPayments RazorPayments { get; set;}
+    }
+}
---
+++ Models/RazorPayPayments.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FindALawyer.Models
+{
+    [Table("razorpay_payments")]
+    public class RazorPayPayments
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RazorPayId { get; set; }
+
+        public string PaymenrOrderId { get; set; } = string.Empty;
+
+        [AllowNull]
+        public string? PaymentId { get; set; } = string.Empty;
+
+        public float Amount { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
