2e94c1789287fca7813dcf1a6bdc7008fe96e3c5
ananthsekar007 <ananthsekar007@gmail.com>
2023-07-11 11:02:52 +0530

    Added Dao class for getting rating

---
+++ Dao/LawyerDao/Rating.cs
@@ -0,0 +1,12 @@
+﻿namespace FindALawyer.Dao.LawyerDao
+{
+    public class Rating
+    {
+        public int LawyerId { get; set; }
+        public int ClientId { get; set; }
+        public int RatingValue { get; set; }
+        
+        public string Remarks { get; set; } = string.Empty;
+
+    }
+}
